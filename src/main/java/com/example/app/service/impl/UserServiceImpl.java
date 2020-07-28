b562ce823c31ac1724daa95b93791fc667d333b2
Marko Smoljanovic <markosmoljanovicbg@gmail.com>
2020-07-28 13:44:59 +0200

    added validator for email

--- src/main/java/com/example/app/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/app/service/impl/UserServiceImpl.java
@@ -1,4 +1,30 @@
 package com.example.app.service.impl;
 
-public class UserServiceImpl {
+import com.example.app.dto.UserDto;
+import com.example.app.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public UserDto save(UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto deleteById(Long id) {
+        return null;
+    }
 }
