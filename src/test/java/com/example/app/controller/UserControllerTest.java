93ca8ef449ac1ecf25b3c81009bfce6cc9a25027
Marko Smoljanovic <markosmoljanovicbg@gmail.com>
2020-07-29 17:20:36 +0200

    added UserControllerTest.java

--- src/test/java/com/example/app/controller/UserControllerTest.java
+++ src/test/java/com/example/app/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.app.controller;
 
+import com.example.app.dto.user.UserDto;
 import com.example.app.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = UserController.class)
@@ -27,9 +34,31 @@ class UserControllerTest {
     private UserService userService;
 
     @Test
-    void findById() throws Exception {
-        mockMvc.perform(get("/user/1")
-                .contentType("application/json"))
-                .andExpect(status().isOk());
+    void findByIdSuccess() throws Exception {
+
+        UserDto userDto = new UserDto();
+        userDto.setId(1L);
+        userDto.setFirstName("John");
+
+        when(userService.findById(anyLong())).thenReturn(userDto);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/user/1")
+                .accept(APPLICATION_JSON);
+
+        MvcResult mvcResult = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String expected = objectMapper.writeValueAsString(userDto);
+        String actual = mvcResult.getResponse().getContentAsString();
+
+        assertThat(actual).isEqualTo(expected);
+
+    }
+
+    @Test
+    void findByIdException() {
+        // TODO
     }
 }
