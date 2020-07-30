3752c9b6840782fa592899594c0b90c69218cc47
Marko Smoljanovic <markosmoljanovicbg@gmail.com>
2020-07-30 14:06:11 +0200

    cleaned

--- src/main/java/com/example/app/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/app/service/impl/CommentServiceImpl.java
@@ -9,8 +9,6 @@ import com.example.app.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class CommentServiceImpl implements CommentService {
 
