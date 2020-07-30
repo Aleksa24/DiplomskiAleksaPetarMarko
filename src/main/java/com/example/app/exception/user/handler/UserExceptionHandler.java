08524bcc1db1a4269a586d5f9034faadcd98aba8
Marko Smoljanovic <markosmoljanovicbg@gmail.com>
2020-07-30 03:50:39 +0200

    added @ExceptionHandler to UserExceptionHandler.java

--- src/main/java/com/example/app/exception/user/handler/UserExceptionHandler.java
+++ src/main/java/com/example/app/exception/user/handler/UserExceptionHandler.java
@@ -1,9 +1,19 @@
 package com.example.app.exception.user.handler;
 
+import com.example.app.exception.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class UserExceptionHandler {
+public class UserExceptionHandler extends ResponseEntityExceptionHandler {
 
-
+    @ExceptionHandler({UserNotFoundException.class})
+    public ResponseEntity<Object> internalServerErrorException(Exception e, WebRequest webRequest) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.OK, webRequest);
+    }
 }
