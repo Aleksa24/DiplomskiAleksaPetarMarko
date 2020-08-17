08cc3877d5b65ca86bd022a12052599de2024bec
Marko Smoljanovic <markosmoljanovicbg@gmail.com>
2020-08-17 20:30:13 +0200

    added Auditing

--- src/main/java/com/example/app/AppApplication.java
+++ src/main/java/com/example/app/AppApplication.java
@@ -3,6 +3,7 @@ package com.example.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,6 +15,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AppApplication {
 
     public static final String CHANNEL_FOLDER = System.getProperty("user.home") + "/DiplomskiAplikacija/channel/";
