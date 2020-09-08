ef8e038d9a88c195c010c22c2d33299a2b61ae3f
Marko Smoljanovic <markosmoljanovicbg@gmail.com>
2020-09-08 04:23:55 +0200

    added properties and constants for CorsFilter

--- src/main/java/com/example/app/config/CorsFilter.java
+++ src/main/java/com/example/app/config/CorsFilter.java
@@ -1,5 +1,6 @@
 package com.example.app.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -10,19 +11,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.example.app.constants.CorsConstant.*;
+
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @WebFilter("/*")
 public class CorsFilter implements Filter {
 
+    @Value("${endpoints.cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Value("${endpoints.cors.allowed-methods}")
+    private String allowedMethods;
+
+    @Value("${endpoints.cors.allow-credentials}")
+    private String allowCredentials;
+
+    @Value("${endpoints.cors.allowed-headers}")
+    private String allowedHeaders;
+
+    @Value("${endpoints.cors.max-age}")
+    private String maxAge;
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+        response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, allowedOrigins);
+        response.setHeader(ACCESS_CONTROL_ALLOW_METHODS, allowedMethods);
+        response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, allowedHeaders);
+        response.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, allowCredentials);
+        response.setHeader(ACCESS_CONTROL_MAX_AGE, maxAge);
+        if (OPTIONS.equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
             chain.doFilter(req, res);
