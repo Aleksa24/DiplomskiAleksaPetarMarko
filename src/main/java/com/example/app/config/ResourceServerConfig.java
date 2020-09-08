21f2e6523cb2f10523d6c1aab4349f6492713f06
Marko Smoljanovic <markosmoljanovicbg@gmail.com>
2020-09-08 03:43:26 +0200

    added CorsFilter

--- src/main/java/com/example/app/config/ResourceServerConfig.java
+++ src/main/java/com/example/app/config/ResourceServerConfig.java
@@ -1,35 +1,10 @@
 package com.example.app.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        return new CorsFilter(httpServletRequest -> {
-            CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-            configuration.setAllowedMethods(Arrays.asList("POST", "GET"));
-
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", configuration);
-            return configuration;
-        });
-    }
 }
