eb747ca9cfb599a96e4e2f86186249f218779e2f
Marko Smoljanovic <markosmoljanovicbg@gmail.com>
2020-09-15 14:28:03 +0200

    added authorization rule

--- src/main/java/com/example/app/config/ResourceServerConfig.java
+++ src/main/java/com/example/app/config/ResourceServerConfig.java
@@ -2,19 +2,24 @@ package com.example.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().mvcMatchers("/channel/*").hasAuthority("read");
+    }
 }
