d96a984b491d4bae1bdeff5c9b7f4b4481b393dd
Marko Smoljanovic <markosmoljanovicbg@gmail.com>
2020-09-16 00:04:39 +0200

    added authorization rule for channel/all

--- src/main/java/com/example/app/config/ResourceServerConfig.java
+++ src/main/java/com/example/app/config/ResourceServerConfig.java
@@ -20,6 +20,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().mvcMatchers("/channel/*").hasAuthority("read");
+                .authorizeRequests().mvcMatchers("/channel/all").hasAuthority("read");
     }
 }
